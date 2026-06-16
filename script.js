8c26617e734e024b82e0dacafad3678c87b3bdaf
NUS-Works <goat0602@gmail.com>
2026-06-17 05:06:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ document.getElementById("sessionModal");
 const closeButton =
 document.getElementById("closeModal");
 
+const saveButton =
+document.getElementById("saveButton");
+
+const totalSessionsElement =
+document.getElementById("totalSessions");
+
+const categoryInput =
+document.getElementById("categoryInput");
+
+const satisfactionInput =
+document.getElementById("satisfactionInput");
+
+const durationInput =
+document.getElementById("durationInput");
+
+const noteInput =
+document.getElementById("noteInput");
+
+let sessions =
+JSON.parse(localStorage.getItem("sessions")) || [];
+
+updateTotalSessions();
+
 startButton.addEventListener("click", () => {
     modal.classList.remove("hidden");
 });
@@ -14,3 +37,38 @@ startButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     modal.classList.add("hidden");
 });
+
+saveButton.addEventListener("click", () => {
+
+    const session = {
+        date: new Date().toLocaleString(),
+        category: categoryInput.value,
+        satisfaction: satisfactionInput.value,
+        duration: durationInput.value,
+        note: noteInput.value
+    };
+
+    sessions.push(session);
+
+    localStorage.setItem(
+        "sessions",
+        JSON.stringify(sessions)
+    );
+
+    updateTotalSessions();
+
+    categoryInput.value = "";
+    durationInput.value = "";
+    noteInput.value = "";
+
+    modal.classList.add("hidden");
+
+    alert("SESSION SAVED");
+});
+
+function updateTotalSessions() {
+
+    totalSessionsElement.textContent =
+        sessions.length;
+
+}
