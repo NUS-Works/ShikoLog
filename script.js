c27951f8ae491c744f30e7963c13270dec4366c3
NUS-Works <goat0602@gmail.com>
2026-06-17 04:37:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const startButton =
+document.querySelector(".main-button");
 
+const modal =
+document.getElementById("sessionModal");
+
+const closeButton =
+document.getElementById("closeModal");
+
+startButton.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+});
+
+closeButton.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
